75512786d6aa733714a542fa9684740d0b1684f9
sakura-0329 <147309375+sakura-0329@users.noreply.github.com>
2024-01-14 12:21:42 +0900

    Add files via upload

---
+++ kadai_005/kadai_005/score.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>5章課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 変数に値を代入する
+        $score_num1 = 80;
+        $score_num2 = 60;
+        $score_num3 = 55;
+        $score_num4 = 40;
+        $score_num5 = 100;
+        $score_num6 = 25;
+        $score_num7 = 80;
+        $score_num8 = 95;
+        $score_num9 = 30;
+        $score_num10 = 60;
+
+        $score_total = $score_num1 + $score_num2 + $score_num3 + $score_num4 + $score_num5 + $score_num6 + $score_num7 + $score_num8 + $score_num9 + $score_num10;  
+        $score_average = $score_total / 10;
+        echo $score_average
+
+
+        ?>
+    </p>
+</body>
+
+</html>
