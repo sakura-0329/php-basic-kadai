b7559f102e6dafb21afa2a6cdfb29e23bfe06638
sakura-0329 <147309375+sakura-0329@users.noreply.github.com>
2024-01-21 09:31:07 +0900

    Add files via upload

---
+++ sort.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    <p>
+        <?php
+
+        function sort_2way($array,$order) {
+            if ($order) {  //引数$orderが昇順ならば
+                echo"昇順にソートします","<br>";
+                sort($array);
+            }else{
+                echo"降順にソートします","<br>";
+                rsort($array);
+            }
+                
+        foreach($array as $num){
+            echo $num,"<br>";
+          }
+        }
+        $nums= [15, 4, 18, 23, 10 ];
+        sort_2way($nums,true);
+              
+        ?>
+    </p>
+</body>
+
+</html>
